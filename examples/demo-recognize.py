6d8100fb5fb6ccb679c4d22fd077182156fa5dd8
Jack Wu <leopardsaga@gmail.com>
2019-07-09 16:11:16 +0800

    Refactor: demo-recognize to load specific model num. Device: load state_dict to CPU.

--- examples/demo-recognize.py
+++ examples/demo-recognize.py
@@ -1,10 +1,31 @@
+import json
+
 import _init_path
+import torch
+
 from models.conv import GatedConv
 
-model = GatedConv.load("pretrained/gated-conv.pth")
+def recognize(epoch_load):
+    with open("./data/labels.json") as f:
+        vocabulary = json.load(f)
+        model = GatedConv(vocabulary)
+        # load to CPU, `map_location=lambda storage, loc: storage`
+        state_dict = torch.load("pretrained/model_{}.pth".format(epoch_load), map_location=lambda storage, loc: storage)
+        model.load_state_dict(state_dict)
+        model.eval()
 
-text = model.predict("test.wav")
+    text = model.predict("test.wav")
 
-print("")
-print("识别结果:")
-print(text)
+    print("")
+    print("识别结果:")
+    print(text)
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(description='train model')
+    parser.add_argument('-m', default=0, type=int, help='saved model to load', )
+    args = parser.parse_args()
+    epoch_load = args.m
+    print('using epoch number {}'.format(epoch_load))
+    recognize(epoch_load)
