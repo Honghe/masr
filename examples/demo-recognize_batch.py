daa575fd4bfde2fac7c1f40ab07d823c7ac6fc6e
Jack Wu <leopardsaga@gmail.com>
2019-07-12 11:45:24 +0800

    Add CER.

--- examples/demo-recognize_batch.py
+++ examples/demo-recognize_batch.py
@@ -1,11 +1,11 @@
+import json
 import os
 
-import json
-
+import pandas as pd
 import torch
 
 from models.conv import GatedConv
-import pandas as pd
+from utils import levenshtein
 
 with open("../data/labels.json") as f:
     vocabulary = json.load(f)
@@ -13,10 +13,13 @@ with open("../data/labels.json") as f:
     model = GatedConv(vocabulary)
     state_dict = torch.load("../pretrained/model_{}.pth".format(158))
     model.load_state_dict(state_dict)
+
+
 def asr(f_path):
     text = model.predict(f_path)
     return text
     text = model.predict(f_path)
+
 
 if __name__ == '__main__':
     # record("record.wav", time=5)  # modify time to how long you want
@@ -24,14 +27,18 @@ if __name__ == '__main__':
     audio_dir = '/home/ubuntu/honghe/data/asr'
     meta_data_file_path = os.path.join(os.path.dirname(__file__), '../data', 'thchs_test.txt')
     meta_data = pd.read_csv(meta_data_file_path, sep='\t', header=None)
+    cers = []
     for i in meta_data.index[:11]:
         print(i)
-        if i==2:
+        if i == 2:
             continue
         filename, pnyn, chars = meta_data.loc[i]
         f_path = os.path.join(audio_dir, filename)
         y_p = asr(f_path)
-        print(''.join(chars.split()))
+        chars = ''.join(chars.split())
+        print(chars)
         print(y_p)
-
-
+        cer = levenshtein(y_p, chars) / len(chars)
+        cers.append(cer)
+        print('cer: {:.3f}'.format(cer))
+    print('avg cer: {:.3f}'.format(sum(cers) / len(cers)))
